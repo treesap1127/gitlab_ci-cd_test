407c92951f4d1ea695c2c16f61395ca48ce161d6
treesap <treesap@mayeye.net>
2025-07-11 11:05:20 +0900

    test

---
+++ src/main/java/net/mayaye/core/SpringAiApplication.java
@@ -0,0 +1,13 @@
+package net.mayaye.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/mayaye/core/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mayaye.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
