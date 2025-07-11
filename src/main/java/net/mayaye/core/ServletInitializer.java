ca8bb92b570d89005e48bc80f121b9dedfec3648
treesap <treesap@mayeye.net>
2025-07-11 13:16:51 +0900

    test

--- src/main/java/net/mayaye/core/ServletInitializer.java
+++ src/main/java/net/mayaye/core/ServletInitializer.java
@@ -7,6 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		System.out.println("test");
 		return application.sources(SpringAiApplication.class);
 	}
 
